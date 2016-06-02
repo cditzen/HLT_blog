e2e9223b640833666d9204573576b74405cac680
cditzen <cditzen@iastate.edu>
2016-06-02 16:33:39 -0500

    More user authentication. Users can sign in and gain full access to the site, or be a guest and view current articles and comments. TODO: Need to distinguish between admin and signed-in user.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,9 +2,6 @@ class ArticlesController < ApplicationController
 
   # http_basic_authenticate_with name: "dhh", password: "lol", except: [:index, :show]
 
-  before_action :authenticate_user!
-
-
   def index
     @article = Article.all
   end
